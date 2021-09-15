c4fb751400e2468d19a0c65d9a00d07e1acfd48a
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2021-09-15 16:09:38 +0530

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port = 8000;
 
 // uses express route
 app.use('/',require('./routes')) // any request is comming required the index of route
-
-
+// setting up view engine for our app
+app.set('view engine','ejs');
+app.set('views','./views')
 
 app.listen(port,function(err){
     if(err){
