816a1ed63b61493cd91cb31868818f9c3e535ebc
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2021-09-30 21:51:26 +0530

    add logic for deleting comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,3 +24,18 @@ module.exports.create = function(req,res){
     
     
 }
+
+// logic for destroying ommnets
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user == req.user.id){
+            let postId = comment.post; // storing comment id of comment.post before deleting andwe are going to use this into deleting the comment id @ postSchema
+            comment.remove();
+            Post.findByIdAndUpdate(postId,{ $pull: { comments: req.params.id }},function(err,post){ // here { $pull: { comments: req.params.id } is used to pull the comments from DB
+                return res.redirect('back');
+            }); 
+        }else{
+                return res.redirect('back');        
+        }
+    })  
+}
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const passport = require('passport');
 const router = express.Router();
 
 const commentController = require('../controllers/comments_controller');
-router.post('/create',passport.checkAuthentication,commentController.create)
+router.post('/create',passport.checkAuthentication,commentController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentController.destroy);
 module.exports = router;
