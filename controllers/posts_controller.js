cae4cdec6e9635c28708f7520d1a1d8ebe265a1b
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2021-10-03 11:23:10 +0530

    added async and await to home and post controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,28 +1,40 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function(req,res){
+module.exports.create =async function(req,res){
    // console.log(req.user); // need to understand properly
-    Post.create({
+   try {
+    await  Post.create({
         content:req.body.content,
         user:req.user._id // this user is omming from app.use(passport.setAuthenticatedUser)
-    },function(err,post){
-        if(err){console.log('error in creating a post'); return;}
-        return res.redirect('/')
     })
+    return res.redirect('back')
+       
+   } catch (error) {
+    console.log('Error',err) ;
+    return;
+   }
+    
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id,function(err,post){ // getting the id from from paran not by using query
+module.exports.destroy =async function(req,res){
+    try {
+        let post=await Post.findById(req.params.id) // getting the id from from paran not by using query
         // putting check for authorizaion i.e am i allow to delete that post i.e if the user has make that post then only he can delete that post not onter then that user that has make that post
         //.id means converting the objet id into string
         if(post.user == req.user.id){ //we are getting user id from post schema kindly check 2.we re not using this req.user._id bacause it is not in string thereofore we hava to use req.user.id
             post.remove(); // removing the post
-            Comment.deleteMany({post: req.params.id},function(err,){ // deleting all the comments by  id
-                return res.redirect('back')
+           await Comment.deleteMany({post: req.params.id}) // deleting all the comments by  id) 
+            
+           return res.redirect('back')
 
-            }) 
+        
         }else { // if post and id donot match
             return res.redirect('back')
-        }
-    })
+        }    
+    } catch (error) {
+        console.log('Error',error)
+    }
+
+    
+    
 }
